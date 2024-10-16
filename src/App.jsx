b89cfff57f96835f3873ef171d0888a3a0cece2f
saeedmar <saeed.maruffy@hotmail.com>
2024-10-16 22:58:37 +0200

    laatste-werk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import PokemonCatalog from "./component/PokemonCatalog.jsx";
 
 function App() {
 
   return (
     <>
       <h1>Gotta catch em all!</h1>
+        <div className="main-container">
+            <PokemonCatalog/>
+        </div>
+
     </>
   )
 }
